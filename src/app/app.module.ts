8bcf6d05928d8e96db591ef3f4269de79e7c7613
Çınar Düzen <cinarduzen@gmail.com>
2018-02-28 21:40:38 +0300

    ilk gönderim

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ReadingListComponent } from './reading-list/reading-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ReadingListComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule
