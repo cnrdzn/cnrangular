ca81f3435152081273af4b93fb9b2db86886dbdd
Çınar Düzen <cinarduzen@gmail.com>
2018-03-01 00:09:00 +0300

    - sayfa düzeni oluşturuldu - sass ayarları yapıldı

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ReadingListComponent } from './reading-list/reading-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    data: { title: 'Ana Sayfa' }
+  },  {
+    path: 'reading-list',
+    component: ReadingListComponent,
+    data: { title: 'Okuma Listesi' }
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
@@ -16,7 +28,11 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } //debug
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/reading-list/reading-list.component.ts
+++ src/app/reading-list/reading-list.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-reading-list',
   templateUrl: './reading-list.component.html',
-  styleUrls: ['./reading-list.component.css']
+  styleUrls: ['./reading-list.component.scss']
 })
 export class ReadingListComponent implements OnInit {
 
