1d6f51c60488373f738c7b7dbdca4a883617720f
Çınar Düzen <cinarduzen@gmail.com>
2018-03-18 22:53:06 +0300

    - tasarım düzenlenmesi yapıldı. - routing yapısı pasifleştirildi.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  works = [
+    {
+    "index": 1,
+    "date": "14.03.2018",
+    "title": "proje 1",
+    "image": "proje1.jpg"
+    },
+    {
+    "index": 2,
+    "date": "15.03.2018",
+    "title": "proje 2",
+    "image": "proje2.jpg"
+    },
+    {
+    "index": 3,
+    "date": "16.03.2018",
+    "title": "proje 3",
+    "image": "proje3.jpg"
+    },
+    {
+    "index": 4,
+    "date": "17.03.2018",
+    "title": "proje 4",
+    "image": "proje4.jpg"
+    }
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { ReadingListComponent } from './reading-list/reading-list.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const appRoutes: Routes = [
-  {
-    path: '',
-    component: HomeComponent,
-    data: { title: 'Ana Sayfa' }
-  },  {
-    path: 'reading-list',
-    component: ReadingListComponent,
-    data: { title: 'Okuma Listesi' }
-  },
-  { path: '**', component: PageNotFoundComponent }
-];
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    ReadingListComponent,
-    PageNotFoundComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    RouterModule.forRoot(
-      appRoutes,
-      { enableTracing: true } //debug
-    )
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
