6afa6dc993d0666c5663884d2b074efa96da8859
Çınar Düzen <cinarduzen@gmail.com>
2018-04-01 23:12:16 +0300

    - sosyal ağlardan son durum için alt yapı ayarlaması yapıldı.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from "rxjs/Observable";
+import {HttpClient} from "@angular/common/http";
+import 'rxjs/add/operator/map';
+import { forOwn } from 'lodash';
 
+
+interface GitHub {
+  type: string;
+  repo:string;
+  payload:string;
+}
+
+interface Posts {
+  title: string;
+  id:string;
+  body:string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  github$: Observable<GitHub[]>;
+
+  posts$: Observable<Posts[]>;
+  constructor(private http:HttpClient) {
+  }
+
+  ngOnInit() {
+      this.github$ = this.http
+          .get<GitHub[]>("https://api.github.com/users/cnrdzn/events")
+          .map(data => forOwn(data))
+          
+      this.posts$ = this.http
+      .get<Posts[]>("https://jsonplaceholder.typicode.com/posts")
+      .map(data => forOwn(data))          
+  }
+
   works = [
     {
     "index": 1,
